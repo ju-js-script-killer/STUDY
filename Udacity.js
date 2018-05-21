350b103be654b42cc0cec925bc8abd524256413f
ju-js-script-killer <julesbronnikova@gmail.com>
2018-05-21 23:35:55 +0300

    hammer

---
+++ Udacity.js
@@ -0,0 +1,7 @@
+var price = 15.00; // price of the hammer
+var money = 20.00; // the amount of money I have
+if ( money >= price){
+  console.log("buy a hammer");
+} else {
+  console.log("don't buy a hammer");
+}
