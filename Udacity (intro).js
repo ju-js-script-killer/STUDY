ddeacf648d278057528709d1c8632a44646c7caf
ju-js-script-killer <38808240+ju-js-script-killer@users.noreply.github.com>
2018-05-23 18:05:29 +0300

    gj

--- Udacity (intro).js
+++ Udacity (intro).js
@@ -1,3 +1,4 @@
+/* Lesson 3. Conditionals. 3. Flowchart to code */
 var price = 15.00; // price of the hammer
 var money = 20.00; // the amount of money I have
 if ( money >= price){
@@ -5,3 +6,30 @@ if ( money >= price){
 } else {
   console.log("don't buy a hammer");
 }
+
+/* Lesson 3. Conditionals. 4. If ... else statements */
+var money = 100.50;
+var price = 100.50;
+
+if (money > price) {
+  console.log("You paid extra, here's your change.");
+} else if (money === price) {
+  console.log("You paid the exact amount, have a nice day!");
+} else {
+  console.log("That's not enough, you still owe me money.");
+}
+/* Example #2 with 2 else if */
+var runner = "Kendyll";
+var position = 2;
+var medal;
+
+if(position === 1) {
+  medal = "gold";
+} else if(position === 2) {
+  medal = "silver";
+} else if(position === 3) {
+  medal = "bronze";
+} else {
+  medal = "pat on the back";
+}
+console.log(runner + " received a " + medal + " medal.");
