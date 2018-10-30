aa3a22bb2416caa3b95ea939111cd54c520847d6
ju-js-script-killer <38808240+ju-js-script-killer@users.noreply.github.com>
2018-10-30 18:53:53 +0300

    new

---
+++ Windows_copy_code.js
@@ -0,0 +1,10 @@
+function showHide () {
+var x = document.getelementById("block_id2");
+ if (x.style.display === "none") {
+    x.style.display = "block"
+ }
+ else {
+   x.style.display = "none";
+
+ }
+}
