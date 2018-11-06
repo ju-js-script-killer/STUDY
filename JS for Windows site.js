8c17c844fcf3fb07d5d446d773b27ec558c5f675
ju-js-script-killer <julesbronnikova@gmail.com>
2018-11-06 22:36:11 +0300

    скоба

--- JS for Windows site.js
+++ JS for Windows site.js
@@ -28,68 +28,66 @@ rightButton.onclick = function() {
 };
 };*/
 window.onload = function() {
-(function() {
-  var sliderInnerDiv = document.querySelector('.container_3 > div');
-  var leftButton = document.querySelector('.left');
-  var rightButton = document.querySelector('.right');
-  var margin = 0;
-  var targetMargin = 0;
-  var blockWidth = sliderInnerDiv.children[0].offsetWidth;
-  var blocksCount = sliderInnerDiv.children.length;
-  var speed = 10;
-  var animateIsActive = false;
+  (function() {
+    var sliderInnerDiv = document.querySelector('.container_3 > div');
+    var leftButton = document.querySelector('.left');
+    var rightButton = document.querySelector('.right');
+    var margin = 0;
+    var targetMargin = 0;
+    var blockWidth = sliderInnerDiv.children[0].offsetWidth;
+    var blocksCount = sliderInnerDiv.children.length;
+    var speed = 10;
+    var animateIsActive = false;
 
-  function animate() {
-    if (!animateIsActive) {
-      animateIsActive = true;
-      var t = setInterval(function() {
-        if (Math.abs(margin - targetMargin) < speed) {
-          margin = targetMargin;
-          clearInterval(t);
-          animateIsActive = false;
-        }
-        else {
-          margin += targetMargin > margin ? speed : -speed;
-        }
-        sliderInnerDiv.style.marginLeft = margin + 'px';
-      }, 3);
+    function animate() {
+      if (!animateIsActive) {
+        animateIsActive = true;
+        var t = setInterval(function() {
+          if (Math.abs(margin - targetMargin) < speed) {
+            margin = targetMargin;
+            clearInterval(t);
+            animateIsActive = false;
+          }
+          else {
+            margin += targetMargin > margin ? speed : -speed;
+          }
+          sliderInnerDiv.style.marginLeft = margin + 'px';
+        }, 3);
+      }
     }
 
+    leftButton.onclick = function() {
+      if (targetMargin < 0) {
+        targetMargin += blockWidth;
+      }
+      animate();
+    };
 
-  leftButton.onclick = function() {
-    if (targetMargin < 0) {
-      targetMargin += blockWidth;
-    }
-    animate();
-  };
-
-  rightButton.onclick = function() {
-    if (targetMargin > -blockWidth * (blocksCount - 1)) {
-      targetMargin -= blockWidth;
-    }
-    animate();
-  };
-})();
-};
+    rightButton.onclick = function() {
+      if (targetMargin > -blockWidth * (blocksCount - 1)) {
+        targetMargin -= blockWidth;
+      }
+      animate();
+    };
+  })();
 
 // Переключение в 6 контейнере
-
   var left_button = document.querySelector('.slide_1_hidden');
   var left_text = document.querySelector('.text_6');
   var left_img = document.querySelector('.container_5');
   var right_button = document.querySelector('.slide_2_hidden');
   var right_text = document.querySelector('.text_6_hidden');
   var right_img = document.querySelector('.container_5_hidden');
-    left_button.onclick = function() {
-       right_text.style.display = 'none';
-       right_img.style.display = 'none';
-       left_text.style.display = 'block';
-       left_img.style.display = 'block';
-    };
-    right_button.onclick = function() {
-       right_text.style.display = 'block';
-       right_img.style.display = 'block';
-       left_text.style.display = 'none';
-       left_img.style.display = 'none';
-    };
+  left_button.onclick = function() {
+     right_text.style.display = 'none';
+     right_img.style.display = 'none';
+     left_text.style.display = 'block';
+     left_img.style.display = 'block';
   };
+  right_button.onclick = function() {
+     right_text.style.display = 'block';
+     right_img.style.display = 'block';
+     left_text.style.display = 'none';
+     left_img.style.display = 'none';
+  };
+};
